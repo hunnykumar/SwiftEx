16fece90c00aac8fdcdeacd98896bd43c139cfed
hunny <hunny@katchintech.com>
2025-04-07 11:33:25 +0530

    BNB swap send for test-net

--- src/Dashboard/constants.js
+++ src/Dashboard/constants.js
@@ -45,11 +45,11 @@ export const faucets ={
 export const jwtSecret = "0xC02aaA3MunziAppTestMode0xbb4CdB9";
 
 export const RPC = {
-  BSCRPC: "https://bnb-testnet.g.alchemy.com/v2/k5oEPTr8Pryz-1bdXyNzH3TfwczQ_TRo",
+  BSCRPC: "https://bsc-testnet.bnbchain.org",
   BSCRPC2:
     // "https://bsc.getblock.io/a011daa0-3099-4f55-b22c-c3a3d55898d0/testnet/", //OLD
     // "https://bsc-testnet.blockpi.network/v1/rpc/public",
-    'https://bnb-testnet.g.alchemy.com/v2/k5oEPTr8Pryz-1bdXyNzH3TfwczQ_TRo',
+    'https://bsc-testnet.bnbchain.org',
   MATICRPC:
     "https://polygon-mumbai.g.alchemy.com/v2/WUJnVsFyDvgwlbSdNrpr-lMg0-NKaNLH",
   // ETHRPC:"https://eth-goerli.g.alchemy.com/v2/E1L1Ovo8ZG9-8UjFrNY39wcjPSa0uYLG",  //OLD
