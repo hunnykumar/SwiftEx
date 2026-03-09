841929e0db13c48432af1d4bd5ae459fa0e0705e
hunnykumar <hunny@katchintech.com>
2026-03-09 09:39:26 +0530

    notification updates.

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ global.process = require('process');
 import { AppRegistry, Platform } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
+import messaging from '@react-native-firebase/messaging';
+import { firebaseNotification } from './src/Dashboard/notifications/firebasePushMessages';
 
+messaging().onMessage(async (remoteMessage) => {
+    firebaseNotification(remoteMessage.notification.title,'SwiftEx',remoteMessage.notification.message,remoteMessage.notification.body,remoteMessage?.notification?.android?.imageUrl,remoteMessage?.data)
+  });
 AppRegistry.registerComponent(Platform.OS==="ios"?"test_app":appName, () => App);
